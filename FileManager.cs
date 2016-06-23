f7019a8e0077ca117cc450fa5d6129478bda760a
mvasilva <mvasilva12@gmail.com>
2016-06-23 15:41:51 -0300

    1.0

---
+++ FileManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvasilva.Miscellaneous
+{
+    public class FileManager
+    {
+
+        public static int ClearFilesByExtension(string folderPath, List<string> extension)
+        {
+
+            List<string[]> filesList = new List<string[]>();
+
+
+            extension.ForEach(e => {
+
+                filesList.Add(Directory.GetFiles(folderPath, string.Format("*.{0}", e)));
+            
+            });
+
+            int filesDeleted = 0;
+
+            foreach (string[] files in filesList)
+            {
+
+                Parallel.ForEach(files, filePath =>
+                {
+
+                    FileInfo file = new FileInfo(filePath);
+
+                    TimeSpan diff = DateTime.Now.Subtract(file.LastWriteTime);
+
+
+                    if (diff.TotalHours >= 1 && file.Exists)
+                    {
+                        try
+                        {
+                            file.Delete();
+
+                            filesDeleted++;
+                        }
+                        catch (Exception)
+                        {
+
+
+                        }
+
+                    }
+
+
+                });
+
+            }
+
+
+            string[] directorys = Directory.GetDirectories(folderPath);
+
+            if (directorys.Length > 0)
+            {
+
+                Parallel.ForEach(directorys, directoryPath => { filesDeleted += ClearFilesByExtension(directoryPath, extension); });
+
+            }
+
+
+            return filesDeleted;
+
+        }
+    }
+}
